19e6bfc19e3181f95262409ca8d210f666eb9bc7
tonmoydevbd <tonmoydevbd@gmail.com>
2026-06-15 07:39:03 +0600

    added all courses page.

---
+++ src/app/courses/page.jsx
@@ -0,0 +1,22 @@
+// import CourseCard from "../../../components/CourseCard";
+
+import CourseCard from "@/components/CourseCard";
+
+
+const AllCourses = async () => {
+
+  const res = await fetch('https://skillsphere-sooty.vercel.app/data.json')
+  const allCourses = await res.json()
+
+  return (
+    <div className="max-w-7xl mx-auto my-15">
+      <h2 className="text-2xl font-bold mt-2">All Courses</h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-10">
+        {allCourses.map(course => <CourseCard course={course} key={course.id}></CourseCard>)}
+      </div>
+    </div>
+  )
+
+}
+
+export default AllCourses;
